65a47ae9d5f0b75a34035ae5b99aecd46b7f01fc
Lupu Daniel <dlupu666@gmail.com>
2023-04-29 09:05:37 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Задача 64: Задайте значение N.
+    Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+    Выполнить с помощью рекурсии.
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10; // значение N
+
+        PrintNumbers(n);
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n > 0)
+        {
+            Console.WriteLine(n);
+            PrintNumbers(n - 1);
+        }
+    }
+}
